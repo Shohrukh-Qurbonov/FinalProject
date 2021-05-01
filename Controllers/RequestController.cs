b548c39ff0adce2fe8376c06d83a555cf62fd08c
Shohrukh <qurbonov.shohrukh@mail.ru>
2021-05-01 14:48:45 +0500

    Finish

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -1,4 +1,8 @@
 ﻿using FinalProject.Context;
+using FinalProject.Context.Entities;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,16 +14,38 @@ namespace FinalProject.Controllers
     public class RequestController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public RequestController(ApplicationDbContext context)
+        public RequestController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             this.context = context;
+            _userManager = userManager;
         }
 
-
-        public IActionResult Index()
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CreateRequestViewModel model)
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+
+            var request = new Request()
+            {
+                Aim = model.Aim,
+                Prepayment = model.Prepayment,
+                CreditSumm = model.CreditSumm,
+                CreditTerm = model.CreditTerm,
+                HomeId = model.HomeId,
+                PhoneNumber = model.PhoneNumber,
+                PurchaseType = model.PurchaseType,
+                RequestDate = DateTime.Now,
+                CurrencyTypeId = model.CurrencyTypeId,
+                FIO = model.FIO
+            };
+
+            await context.Requests.AddAsync(request);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Models/CreateRequestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class CreateRequestViewModel
+    {
+        public string PhoneNumber { get; set; }
+        public string FIO { get; set; }
+        public string PurchaseType { get; set; }
+        public int HomeId { get; set; }
+        public string Aim { get; set; }
+        public int? CreditTerm { get; set; }
+        public double? CreditSumm { get; set; }
+        public double? Prepayment { get; set; }
+        public int CurrencyTypeId { get; set; }
+    }
+}
